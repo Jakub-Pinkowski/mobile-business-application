d6031269f676ba54efbbf3a5d93704b683fe7f15
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-11-24 18:19:32 +0100

    colors

--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -8,7 +8,7 @@ export const Colors = {
   light: {
     text: '#11181C',                       // Dark gray - text color for light mode
     background: '#fff',                    // White - background color for light mode
-    tabBackground: '#0C171D',              // White - background color for light mode
+    tabBackground: '#0C171D',              // Dark - background color for light mode
     primary: '#2a9d8f',                    // Teal - primary color for light mode
     secondary: '#e9c46a',                  // Yellow - secondary color for light mode
     tertiary: '#f4a261',                   // Orange - tertiary color for light mode
@@ -21,6 +21,7 @@ export const Colors = {
   dark: {
     text: '#ECEDEE',                       // Light gray - text color for dark mode
     background: '#151718',                 // Dark gray - background color for dark mode
+    tabBackground: '#0C171D',              // Dark - background color for dark mode
     primary: '#2a9d8f',                    // Teal - primary color for dark mode
     secondary: '#e9c46a',                  // Yellow - secondary color for dark mode
     tertiary: '#f4a261',                   // Orange - tertiary color for dark mode
